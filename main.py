750c5429cede0d5bbf723c82c70bf13f31e29c6d
kirrog <rroogg76aacchh@gmail.com>
2024-10-12 15:03:20 +0300

    [feature]: endpoints

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# LIST OF arms
+
+## LIST OF arms
 # get image list
 # post load image
 # post load images
@@ -11,6 +12,41 @@ app = Flask(__name__)
 # post add bounding box
 # post update bounding box
 
-@app.route("/")
+@app.route("/check", methods=['GET'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/get_image_list", methods=['GET'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/post_load_image", methods=['POST'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/post_load_images", methods=['POST'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/post_dump_results", methods=['POST'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/get_image", methods=['GET'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/post_add_bounding_box", methods=['POST'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/post_update_bounding_box", methods=['POST'])
 def hello_world():
     return "<p>Hello, World!</p>"
