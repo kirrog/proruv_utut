c22f40d769aa4b9b014ad6bceed4da9110b9b522
kirrog <kirrog@zertan.ru>
2024-10-12 15:21:11 +0300

    [feature]: docker compose

--- backend/main.py
+++ backend/main.py
@@ -13,40 +13,44 @@ app = Flask(__name__)
 # post update bounding box
 
 @app.route("/check", methods=['GET'])
-def hello_world():
+def endpoint_check():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/get_image_list", methods=['GET'])
-def hello_world():
+def endpoint_get_image_list():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/post_load_image", methods=['POST'])
-def hello_world():
+def endpoint_post_load_image():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/post_load_images", methods=['POST'])
-def hello_world():
+def endpoint_post_load_images():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/post_dump_results", methods=['POST'])
-def hello_world():
+def endpoint_post_dump_results():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/get_image", methods=['GET'])
-def hello_world():
+def endpoint_get_image():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/post_add_bounding_box", methods=['POST'])
-def hello_world():
+def endpoint_post_add_bounding_box():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/post_update_bounding_box", methods=['POST'])
-def hello_world():
+def endpoint_post_update_bounding_box():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
